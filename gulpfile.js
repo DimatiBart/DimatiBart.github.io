66e72e44a880896d424f940688348cd14d5da164
DimatiBart <dimati.bart@gmail.com>
2016-09-23 17:30:27 +0300

    done one-way

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var less = require('gulp-less');
 var gutil = require('gulp-util');
+var concat = require('gulp-concat');
 
 gulp.task('default', function(){
     gulp.start('buildStyles');
+    gulp.start('buildJS');
 });
 
 gulp.task('buildStyles', function(){
@@ -15,7 +17,13 @@ gulp.task('buildStyles', function(){
         .pipe(rename('main.min.css'))
         .pipe(gulp.dest('./dest/css/'));
 });
+gulp.task('buildJS', function() {
+    gulp.src("./src/js/*.js")
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest('./dest/js/'));
+});
 
 gulp.task('watch', function(){
-    gulp.watch('./src/less/*.less', ['buildStyles'])
+    gulp.watch('./src/less/*.less', ['buildStyles']);
+    gulp.watch('./src/js/*.js', ['buildJS']);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,71 @@
-$(document).on('click', 'ticket',function(){
-    if (!this.classList.contains('no-results')){
-        console.log('Hello');
+var touchHelper = {
+    lastTouchPos: {
+        x: null,
+        y: null
+    },
+    saveTouchPosition: function(event){
+        var touch = event.originalEvent.touches[0];
+        this.lastTouchPos.x = touch.pageX;
+        this.lastTouchPos.y =  touch.pageY;
+    },
+    deleteTicketHover: function () {
+        $('.ticket.hovered').removeClass('hovered');
+        $('.date-cell.hovered').removeClass('hovered');
     }
+};
+
+$(function(){
+    var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0) || (navigator.MaxTouchPoints > 0));
+    if (isTouch){
+        $('body').addClass('touchDevice');
+
+        $(document).on('touchstart', '.ticket',function(event){
+            touchHelper.saveTouchPosition.call(touchHelper, event);
+            touchHelper.deleteTicketHover ();
+            $this = $(this);
+            $this.addClass('hovered');
+            var index = $this.index() + 1;
+            $('.dates-container .date-cell:nth-child('+ index +')').addClass('hovered');
+        });
+
+        $(document).on('touchmove', '.ticket',function(event){
+            touchHelper.saveTouchPosition.call(touchHelper, event);
+        });
+
+        $(document).on('touchend', '.ticket',function(event){
+            $this = $(this);
+            var index = $this.index() + 1;
+            var endTarget = $(document.elementFromPoint(touchHelper.lastTouchPos.x, touchHelper.lastTouchPos.y)).closest('.ticket');
+            if (!endTarget.hasClass('hovered')) {
+                touchHelper.deleteTicketHover ();
+            }
+
+        });
+
+        $(document).on('click', '.ticket',function(){
+            event.preventDefault();
+            if (!this.classList.contains('no-results')){
+                console.log('Hello');
+            }
+        });
+    }
+    else {
+        $(document).on('mouseenter', '.ticket',function(){
+            var index = $(this).index() + 1;
+            $('.dates-container .date-cell:nth-child('+ index +')').addClass('hovered');
+        });
+
+        $(document).on('mouseleave', '.ticket',function(){
+            var index = $(this).index() + 1;
+            $('.date-cell.hovered').removeClass('hovered');
+        });
+
+        $(document).on('click', '.ticket',function(){
+            if (!this.classList.contains('no-results')){
+                console.log('Hello');
+            }
+        });
+    }
+
+
 });
