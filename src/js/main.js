9179d58ce4b0c1b2f83ba2c68b901fa17cf23d69
DimatiBart <dimati.bart@gmail.com>
2016-09-26 15:41:38 +0300

    done buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var touchHelper = {
+var mobileHelper = {
     lastTouchPos: {
         x: null,
         y: null
@@ -11,86 +11,87 @@ var touchHelper = {
     deleteTicketHover: function () {
         $('.ticket.hovered').removeClass('hovered');
         $('.date-cell.hovered').removeClass('hovered');
+    },
+    _tabletDeviceCheck: function(){
+        var metaTag = '<meta name="viewport" content="width=1000">';
+        var isMobile = {
+            Android: function() {
+                return navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function() {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function() {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function() {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function() {
+                return navigator.userAgent.match(/IEMobile/i);
+            },
+            any: function() {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            }
+        };
+        if( isMobile.any() && window.matchMedia("(min-width: 768px)").matches ){
+            $('head').append(metaTag);
+        }
     }
 };
 
-(function tabletDevicesCheck(){
-    var metaTag = '<meta name="viewport" content="width=1000">';
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+var UIController = {
+    initUI: function(mobileHelper){
+        mobileHelper._tabletDeviceCheck();
+        var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0) || (navigator.MaxTouchPoints > 0));
+        if (isTouch){
+            $('body').addClass('touchDevice');
+
+            $(document).on('touchstart', '.flex-results-wrapper .ticket',function(event){
+                mobileHelper.saveTouchPosition.call(mobileHelper, event);
+                mobileHelper.deleteTicketHover ();
+                var $this = $(this);
+                $this.addClass('hovered');
+                var index = $this.index() + 1;
+                $('.dates-container .date-cell:nth-child('+ index +')').addClass('hovered');
+            });
+
+            $(document).on('touchmove', '.ticket',function(event){
+                mobileHelper.saveTouchPosition.call(mobileHelper, event);
+            });
+
+            $(document).on('touchend', '.flex-results-wrapper  .ticket',function(event){
+                var endTarget = $(document.elementFromPoint(mobileHelper.lastTouchPos.x, mobileHelper.lastTouchPos.y)).closest('.ticket');
+                if (!endTarget.hasClass('hovered')) {
+                    mobileHelper.deleteTicketHover ();
+                }
+
+            });
+
+            $(document).on('click', '.ticket',function(){
+                event.preventDefault();
+                if (!this.classList.contains('no-results')){
+                    console.log('sup m8');
+                }
+            });
         }
-    };
-    if( isMobile.any() && window.matchMedia("(min-width: 768px)").matches ){
-        $('head').append(metaTag);
+        else {
+            $(document).on('mouseenter', '.flex-results-wrapper  .ticket',function(){
+                var index = $(this).index() + 1;
+                $('.dates-container .date-cell:nth-child('+ index +')').addClass('hovered');
+            });
+
+            $(document).on('mouseleave', '.flex-results-wrapper .ticket',function(){
+                $('.date-cell.hovered').removeClass('hovered');
+            });
+
+            $(document).on('click', '.ticket',function(){
+                if (!this.classList.contains('no-results')){
+                    console.log('sup m8');
+                }
+            });
+        }
     }
-})();
+};
 
-(function initUI(){
-    var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0) || (navigator.MaxTouchPoints > 0));
-    if (isTouch){
-        $('body').addClass('touchDevice');
-
-        $(document).on('touchstart', '.flex-results-wrapper .ticket',function(event){
-            touchHelper.saveTouchPosition.call(touchHelper, event);
-            touchHelper.deleteTicketHover ();
-            $this = $(this);
-            $this.addClass('hovered');
-            var index = $this.index() + 1;
-            $('.dates-container .date-cell:nth-child('+ index +')').addClass('hovered');
-        });
-
-        $(document).on('touchmove', '.ticket',function(event){
-            touchHelper.saveTouchPosition.call(touchHelper, event);
-        });
-
-        $(document).on('touchend', '.flex-results-wrapper  .ticket',function(event){
-            $this = $(this);
-            var index = $this.index() + 1;
-            var endTarget = $(document.elementFromPoint(touchHelper.lastTouchPos.x, touchHelper.lastTouchPos.y)).closest('.ticket');
-            if (!endTarget.hasClass('hovered')) {
-                touchHelper.deleteTicketHover ();
-            }
-
-        });
-
-        $(document).on('click', '.ticket',function(){
-            event.preventDefault();
-            if (!this.classList.contains('no-results')){
-                console.log('Hello');
-            }
-        });
-    }
-    else {
-        $(document).on('mouseenter', '.flex-results-wrapper  .ticket',function(){
-            var index = $(this).index() + 1;
-            $('.dates-container .date-cell:nth-child('+ index +')').addClass('hovered');
-        });
-
-        $(document).on('mouseleave', '.flex-results-wrapper .ticket',function(){
-            var index = $(this).index() + 1;
-            $('.date-cell.hovered').removeClass('hovered');
-        });
-
-        $(document).on('click', '.ticket',function(){
-            if (!this.classList.contains('no-results')){
-                console.log('Hello');
-            }
-        });
-    }
-})();
+UIController.initUI(mobileHelper);
