fc17736978a37bc5a9e8dbaf84174cb433acb99d
DimatiBart <dimati.bart@gmail.com>
2017-04-26 13:24:59 +0300

    uploaded little vr demo

--- vr-demo/utils/setAttr.js
+++ vr-demo/utils/setAttr.js
@@ -1,3 +1,4 @@
+//set attribute as plain object rather than lots of settAttribute calls
 export default (elem, attrObj) => {
     if (!(elem instanceof Node)) {
         throw new Error("Not a Node element");
